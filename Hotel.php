57788fdda4d237671aed55534996b2f0ecac290b
sofian shady <156546065+SofianShady@users.noreply.github.com>
2024-02-18 19:31:51 +0200

    Rating Functionality
    Implementing the Rating Functionality in the Hotel Class
    Based on the tests I've written, now implement the necessary functionality in the Hotel class. This class should now be able to add ratings, store them, and calculate an average rating.

--- Hotel.php
+++ Hotel.php
@@ -4,10 +4,25 @@ class Hotel {
     public $name;
     public $location;
     public $price;
+     public $ratings = [];
 
     public function __construct($name, $location, $price) {
         $this->name = $name;
         $this->location = $location;
         $this->price = $price;
     }
+
+    public function addRating($rating) {
+    if ($rating < 1 || $rating > 5) {
+        throw new Exception("Rating must be between 1 and 5.");
+    }
+    $this->ratings[] = $rating;
+}
+
+public function getAverageRating() {
+    if (empty($this->ratings)) {
+        return "Not rated yet";
+    }
+    return round(array_sum($this->ratings) / count($this->ratings), 2);
+}
 }
